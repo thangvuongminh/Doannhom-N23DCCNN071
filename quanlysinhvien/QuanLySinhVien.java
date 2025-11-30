13ae661da8e5c2038829b84727d33e01cb7e2f8e
Hồ Ngọc Hoàng Anh <149872171+Titapie@users.noreply.github.com>
2025-11-30 19:21:36 +0700

    Update QuanLySinhVien.java

--- quanlysinhvien/QuanLySinhVien.java
+++ quanlysinhvien/QuanLySinhVien.java
@@ -1,12 +1,11 @@
 package quanlysinhvien;
 
-import ui.QuanLySinhVienFrame; // NHỚ IMPORT cái form Swing tao tạo cho bạn
+import ui.QuanLySinhVienFrame; 
 
 public class QuanLySinhVien {
 
     public static void main(String[] args) {
 
-        // Chạy UI Swing trên luồng EDT
         javax.swing.SwingUtilities.invokeLater(() -> {
             new QuanLySinhVienFrame().setVisible(true);
         });
